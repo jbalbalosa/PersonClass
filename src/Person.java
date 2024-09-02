88b80f8924ed6aa534feaa4f2db654787a42c594
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-02 12:58:30 +0900

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,55 @@
 package PACKAGE_NAME;
 
 public class Person {
+
+    private String firstName = null;
+    private String lastName = null;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName(){
+        String fullName = "John Smith";
+
+        if (firstName.isEmpty()){
+            fullName = lastName;
+        }
+
+        if(lastName.isEmpty()){
+            fullName = firstName;
+        }
+
+        return fullName;
+    }
+
+    public boolean isTeen(){
+        return (age > 12 && age < 20);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0 || age > 100){
+            this.age = 0;
+        }else {
+            this.age = age;
+        }
+    }
 }
+
